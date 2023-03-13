40c3e3c839e93aa481b9fb34b74b82d5902085ff
Ethen <ebot14rocks@gmail.com>
2023-03-13 12:43:36 -0400

    centered pit scouting form and removed reports for rn since it prints out as tables

--- reports.js
+++
@@ -1,8 +0,0 @@
-//Create PDF Reports for Event Scouting
-const PDFDocument = require('pdfkit-table');
-const db = require('./my-sql');
-const fs = require('fs');
-
-
-
-//module.exports = { createTeamListPDF, createPitInfoPDF, createMatchInfoPDF};
